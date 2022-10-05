b70fd1bf0c86471acfdcafeda154ff6cfc56f47f
zihaofoo <zihaofoo@mit.edu>
2022-10-04 21:59:38 -0400

    10/4 ZHF Push 3

--- Q3c_linear_map.m
+++ Q3c_linear_map.m
@@ -5,7 +5,7 @@ clc
 %% Variable definition
 start_x = 0.0;
 end_x = 1.0;
-num_x = 1000;           
+num_x = 200;           
 
 mu_F = -2.0;
 sigma_F = sqrt(0.5);
@@ -17,7 +17,7 @@ source = 5.0;           % s(x) = 5
 rightbc = 1.0;          % u_r = 1
 x_loc = 0.6;            % Location of random variable
 
-num_MC = 1E+4; 
+num_MC = 1E+3; 
 
 F = zeros(num_MC, 1); 
 Y1 = zeros(num_MC, 1); 
@@ -25,7 +25,7 @@ Y2 = zeros(num_MC, 1);
 Y3 = zeros(num_MC, 1); 
 Y4 = zeros(num_MC, 1); 
 u_val_vec = zeros(num_MC, 1); 
-u_0 = 40;
+
 
 %% Monte Carlo Estimator
 
@@ -35,6 +35,7 @@ for i1 = 1:num_MC
     Y2(i1) = normrnd(mu_Y, sigma_Y, 1);
     Y3(i1) = normrnd(mu_Y, sigma_Y, 1);
     Y4(i1) = normrnd(mu_Y, sigma_Y, 1);
+   
     u_val_vec(i1) = Q3_solver_lin_regression(x_loc, [F(i1), Y1(i1), Y2(i1), Y3(i1), Y4(i1)]);
 end
         
@@ -42,33 +43,21 @@ end
 x0 = [1, 1, 1, 1, 1, 1]; 
 fun = @(x_vec) lin_regression_sub(x_vec, Y1, Y2, Y3, Y4, F, u_val_vec);
 options_set = optimoptions(@fsolve, 'Algorithm', 'trust-region', 'Display','iter', 'FunctionTolerance', 1E-16);
-x_params = fsolve(fun, x0, options_set) 
+% x_params = fsolve(fun, x0, options_set); 
 
-u_est = solver_linear_map(x_params, Y1, Y2, Y3, Y4, F);
+scal_constant = 10;
+x_params = [2.0637, -0.0807, -0.0683, -0.0654, -0.0610, -1.1237];
+W = solver_linear_map(x_params, Y1, Y2, Y3, Y4, F);
 
-u_err = mean(abs(u_est - u_val_vec))
+u_err = (mean(W) - mean(u_val_vec)) /  mean(u_val_vec);
+mu_W = x_params(1) + (x_params(2) * mu_Y) + (x_params(3) * mu_Y) + (x_params(4) * mu_Y) + (x_params(5) * mu_Y) + (x_params(6) * mu_F); 
+%% Control Variate 
 
-%{
-figure(1)
-plot(num_MC, p_vec, 'k - o', 'LineWidth', 2)
-ylabel('Expected value p from MC simulation', 'FontSize', 18)
+cov_U_W = mean(u_val_vec .* W) - (mean(u_val_vec) * mean(W));
+c_ast = - cov_U_W / var(W);  
+Z = W + c_ast .* (W - mu_W); 
 
-yyaxis right
-plot(num_MC, sqrt(var_vec), 'r - o', 'LineWidth', 2)
-xlabel('Number of sample points in MC simulation', 'FontSize', 18)
-ylabel('Standard error of p from MC simulation', 'FontSize', 18)
-axis('square')
-% Expectation of u(x=0.6)
-% figure(1)
-% plot((num_MC), (h_n))
-% axis('square')
-% xlabel('Number of sample points in MC simulation', 'FontSize', 18)
-% ylabel('Expected value from MC simulation', 'FontSize', 18)
+mean(Z)
+var(u_val_vec)
+var(Z)
 
-% Variance of u(x=0.6)
-% figure(2)
-% plot((num_MC), (var_val_vec), 'k- ^', 'LineWidth', 2)
-% axis('square')
-% xlabel('Number of sample points in MC simulation', 'FontSize', 18)
-% ylabel('Expected variance from MC simulation', 'FontSize', 18)
-%}
