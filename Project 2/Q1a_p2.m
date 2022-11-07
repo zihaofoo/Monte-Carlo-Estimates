f85e29f482733f69d5bf00beeb8cc2bbdbfc691f
zihaofoo <zihaofoo@mit.edu>
2022-11-07 17:02:01 -0500

    11/7 ZHF Push 4

--- Project 2/Q1a_p2.m
+++ Project 2/Q1a_p2.m
@@ -8,7 +8,7 @@ num_points_n = 20;
 num_points_p = 5;
 
 % n_dim = [1, 3, 6, 11, 13, 16, 21];
-n_dim = 10;
+n_dim = 5;
 % n_dim = linspace(1, num_points_n, num_points_n);
 % p_deg = linspace(1, num_points_p, num_points_p);
 p_deg = [0, 1, 2, 3, 4, 5];
@@ -27,12 +27,14 @@ for i1 = 1:length(n_dim)
         k_sample = k_TD_PCE(x_pos, n_dim(i1), p_deg(i2), mu, num_samples);
         y_plot(i1, i2) = abs(mean(k_sample));
         y_var(i1, i2) = abs(var(k_sample));
+        i2
     end
 
 end
 
 plot(p_deg, (y_plot), '^ -', 'LineWidth', 2, 'MarkerSize', 8)
 plot(p_deg, (y_var), 'v -', 'LineWidth', 2, 'MarkerSize', 8)
+plot([0, 5], [exp(1), exp(1)], '-- k' )
 
 
 xlabel('Number of Polynomial Degree, P')
